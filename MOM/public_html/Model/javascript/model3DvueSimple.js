010de5f0f499fe9bc30c54e78c0b8c84b2ddced2
BryanBoni <bryan.boni@etu.univ-lyon1.fr>
2016-06-07 14:39:24 +0200

    update 07/06/2016

---
+++ MOM/public_html/Model/javascript/model3DvueSimple.js
@@ -0,0 +1,111 @@
+
+    if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
+    var container, camera, camLight, scene, renderer;
+    var mouseX = 0, mouseY = 0;
+    var SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = window.innerHeight;
+    var windowHalfX = window.innerWidth / 2, windowHalfY = window.innerHeight / 2;
+    // Model file OBJ
+    var file = '../Ressources/obj3D/MA5.obj';
+    var textureFile = '../Ressources/obj3D/MA5_0.jpg';
+
+    var raycaster = new THREE.Raycaster();
+    var mouse = new THREE.Vector2(), offset = new THREE.Vector3(), INTERSECTED, SELECTED;
+
+    init();
+    animate();
+    function init() {
+        container = document.getElementById( 'container' );
+        camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 5000 );
+        camera.position.z = 600;
+
+        //manager
+        var manager = new THREE.LoadingManager();
+        manager.onProgress = function (item, loaded, total) {
+            console.log(item, loaded, total);
+        };
+        manager.onLoad = function () {
+            console.log('all items loaded');
+            allItemsLoaded();
+        };
+        manager.onError = function () {
+            console.log('there has been an error');
+        };
+
+        //renderer
+        renderer = new THREE.WebGLRenderer( { alpha: true } ); // init like this
+        renderer.setClearColor( 0xffffff, 0 ); // second param is opacity, 0 => transparent
+        renderer.setPixelRatio( window.devicePixelRatio );
+        renderer.domElement.setAttribute("id","charlyTroisD");
+        container.appendChild( renderer.domElement );
+        window.addEventListener( 'resize', onWindowResize, false );
+
+        timerTask();
+
+        scene = new THREE.Scene();
+        var ambient = new THREE.AmbientLight( 0x404040 );
+        scene.add( ambient );
+        var directionalLight = new THREE.DirectionalLight( /*0xffeedd*/0xffffff , 0.5 );
+        directionalLight.position.set( 0, 0, 1 );
+        scene.add( directionalLight );
+        var directionalLight = new THREE.DirectionalLight( 0xffffff , 0.5 );
+        directionalLight.position.set( 0, 0, -1 );
+        scene.add( directionalLight );
+
+        var onProgress = function ( xhr ) {
+            if ( xhr.lengthComputable ) {
+                var percentComplete = xhr.loaded / xhr.total * 100;
+                console.log( Math.round(percentComplete, 2) + '% downloaded' );
+                document.getElementById('pourcent').innerHTML = Math.round(percentComplete, 2) + '% downloaded';
+            }
+        };
+        var onError = function ( xhr ) {};
+        // application des textures
+        var texture = new THREE.Texture();
+        var loader = new THREE.ImageLoader( manager );
+        loader.load( textureFile, function ( image ) {
+            texture.image = image;
+            texture.needsUpdate = true;
+        });
+        var loader = new THREE.OBJLoader( manager );
+        loader.load( file, function ( object ) {
+            object.traverse( function ( child ) {
+                if ( child instanceof THREE.Mesh ) {
+                    child.material.map = texture;
+                }
+            });
+            object.position.y = 0;
+            scene.add( object );
+        }, onProgress, onError );
+    }
+    function timerTask(){
+        $(document).ready(function () {
+            var cl = new CanvasLoader('loading');
+            cl.setColor('#4f4f4f'); // default is '#000000'
+            cl.setShape('spiral'); // default is 'oval'
+            cl.setDiameter(63); // default is 40
+            cl.setDensity(14); // default is 40
+            cl.setRange(1.3); // default is 1.3
+            cl.setSpeed(1); // default is 2
+            cl.setFPS(20); // default is 24
+            cl.show(); // Hidden by default
+        });
+    }
+    function onWindowResize() {
+        windowHalfX = window.innerWidth / 2;
+        windowHalfY = window.innerHeight / 2;
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+    }
+    function animate() {
+        requestAnimationFrame( animate );
+        render();
+    }
+    function render() {
+        renderer.render( scene, camera );
+    }
+    function allItemsLoaded() {
+        $('.onepix-imgloader').hide();
+    }
+    function refreshLoaded() {
+        $('.onepix-imgloader').show();
+    }
--- MOM/public_html/Vue/layout.php
+++ MOM/public_html/Vue/layout.php
@@ -13,74 +13,73 @@ Ici se trouve le layout (template) créé pour servire de structure au autres pa
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" >
 
-        <link rel="stylesheet" href="../Vue/style/css3D.css" >
         <!-- Optional theme -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css">
         <!-- Latest compiled and minified JavaScript -->
         <script src = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-        <?php echo $head1?>
-        
+        <?php echo $head1 ?>
+
+
+        <link rel="stylesheet" type="text/css" href="../Vue/style/main.css">
+        <link rel="stylesheet" type="text/css" href="../Vue/style/css3D.css">
+        <link rel="stylesheet" type="text/css" href="../Vue/style/zoomGallerie.css">
     </head>
-    <link rel="stylesheet" type="text/css" href="../Vue/style/main.css">
-    <link rel="stylesheet" type="text/css" href="../Vue/style/css3D.css">
-    <link rel="stylesheet" type="text/css" href="../Vue/style/zoomGallerie.css">
-</head>
-<body>
-    <a href="#" class="back-to-top hidden-xs">Back to Top</a>
-    <div id = "header">
-        <a href = "http://www.arar.mom.fr/"><img src = "../Ressources/image.gif" style = "width: 400px; height: 60px;"/></a><!-- Need to create  a custom one -->
-        <nav class="navbar navbar-inverse" id = "navBar">
-            <div class="container-fluid" id = "menuBar">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="../index.php" style="font-size: 24px;">Accueil</a>
-                </div>
+    <body>
+        <a href="#" class="back-to-top hidden-xs">Back to Top</a>
+        <div id = "header">
+            <a href = "http://www.arar.mom.fr/"><img src = "../Ressources/image.gif" style = "width: 400px; height: 60px;"/></a><!-- Need to create  a custom one -->
+            <nav class="navbar navbar-inverse" id = "navBar">
+                <div class="container-fluid" id = "menuBar">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="../index.php" style="font-size: 24px;">Accueil</a>
+                    </div>
 
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <li style="text-align: center;"><a href="rechAvc.php">Recherche Avancée</a></li>
-                        <li style="text-align: center;"><a href="../Controlleur/Interne.php">Interne </a></li>
-                    </ul>
-                    </ul>
-                    <ul class="nav navbar-nav navbar-right">
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <li style="text-align: center;"><a href="rechAvc.php">Recherche Avancée</a></li>
+                            <li style="text-align: center;"><a href="../Controlleur/Interne.php">Interne </a></li>
+                        </ul>
+                        </ul>
+                        <ul class="nav navbar-nav navbar-right">
 
-                        <li>
-                            <form class="navbar-form navbar-left" role="search">
-                                <div class="form-group" >
-                                    <input type="text" class="form-control" placeholder="Ajouter à la recherche" name ="search"/>
-                                </div>                    
-                                <button type="submit" class="btn btn-default " ><b class = "glyphicon glyphicon-search"></b></button>
-                            </form>
-                        </li>
+                            <li>
+                                <form class="navbar-form navbar-left" role="search">
+                                    <div class="form-group" >
+                                        <input type="text" class="form-control" placeholder="Ajouter à la recherche" name ="search"/>
+                                    </div>                    
+                                    <button type="submit" class="btn btn-default " ><b class = "glyphicon glyphicon-search"></b></button>
+                                </form>
+                            </li>
 
-                        <li style="text-align: center;"><a href="../Controlleur/connexion.php"><p class="glyphicon glyphicon-user" style="margin: 0px;"> </p><p class ="hidden-sm hidden-md hidden-lg">Utilisateur</p></a></li>
-                    </ul>
-                </div><!-- /.navbar-collapse -->
+                            <li style="text-align: center;"><a href="../Controlleur/connexion.php"><p class="glyphicon glyphicon-user" style="margin: 0px;"> </p><p class ="hidden-sm hidden-md hidden-lg">Utilisateur</p></a></li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
                 </div><!-- /.container-fluid -->
-        </nav>
-    </div>
-    <div class = "container-fluid">
-        <div id = "corps" class = "col-sm-10 col-sm-offset-1">
-            <?php echo $content ?>
+            </nav>
         </div>
-    </div>
-    <br /><br /><br /><br />
+        <div class = "container-fluid">
+            <div id = "corps" class = "col-sm-10 col-sm-offset-1">
+                <?php echo $content ?>
+            </div>
+        </div>
+        <br /><br /><br /><br />
 
-    
-    <div id = "footer">
-        2016 <b>Laboratoire ArAr. Archéologie et Archéométrie</b> - Tout droit réservé. Créé par <b>Bryan Boni</b> & <b>Gabriel Coutu</b>
-    </div>
-    
-    <!-- IMPORTANT BUT NOT USED
-    <script src = "../Model/javascript/file.js"></script>-->
-    <?php echo $head ?>
-    <script src="../Model/javascript/backOnTop.js"></script>
-</body>
+
+        <div id = "footer">
+            2016 <b>Laboratoire ArAr. Archéologie et Archéométrie</b> - Tout droit réservé. Créé par <b>Bryan Boni</b> & <b>Gabriel Coutu</b>
+        </div>
+
+        <!-- IMPORTANT BUT NOT USED
+        <script src = "../Model/javascript/file.js"></script>-->
+        <?php echo $head ?>
+        <script src="../Model/javascript/backOnTop.js"></script>
+    </body>
 </html>
